b934cefe1c7a82c84101e5b0f4b7b69ca64f8027
calebhuang <calebhuang0206@gmail.com>
2025-01-13 10:34:56 -0800

    get /users and added nodemon

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,10 +1,45 @@
 // backend.js
 import express from "express"; // import express
 
+const users = {
+    users_list: [
+        {
+            id: "xyz789",
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "Professor"
+        },
+        {
+            id: "yat999",
+            name: "Dee",
+            job: "Aspring actress"
+        },
+        {
+            id: "zap555",
+            name: "Dennis",
+            job: "Bartender"
+        }
+    ]
+};
+
 const app = express(); // create an instance of express
 const port = 8000; // constant of port 
 
 app.use(express.json()); // process incoming data in JSON format
+
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
