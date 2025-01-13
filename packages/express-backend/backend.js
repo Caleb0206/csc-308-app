3039dae0c3d55f9e46438af0a327403ff3363354
calebhuang <calebhuang0206@gmail.com>
2025-01-13 10:53:47 -0800

    addd POST function to addUser

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,6 +44,17 @@ const findUserByName = (name) => {
 const findUsersById = (id) =>
     users["users_list"].find((user) => user["id"] === id); //returns the first occurrence that matches the condition
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+}
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
 app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; // req.params.id
     let result = findUsersById(id);
